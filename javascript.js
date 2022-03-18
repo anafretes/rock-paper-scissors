26a31160501dc9e8fa68a6b22cf19063f8d588e0
Ana Fretes <anafretes@fpuna.edu.py>
2022-03-18 20:22:40 -0300

    Revisit challenge

--- javascript.js
+++ javascript.js
@@ -1,83 +1,151 @@
-
 function computerSelection() {
-  return Math.floor(Math.random()*3);
+  const selection =  Math.floor(Math.random()*3);
+  return selection;
 }
 
-function playerSelection() {
-  const playerThrow = prompt("Rock, paper, or scissors?", "").toLowerCase();
+function playerSelection(e) {
+  const playerThrow = e.target.textContent.toLowerCase();
   let selected = 0;
+  let playerValue = "The player's choice was ";
   if (playerThrow === 'rock') {
+    playerValue += playerThrow;
+    playerPara.textContent = playerValue;
     return selected = 0;
   } else if (playerThrow === 'paper') {
+    playerValue += playerThrow;
+    playerPara.textContent = playerValue;
     return selected = 1;
   } else if (playerThrow === 'scissors') {
+    playerValue += playerThrow;
+    playerPara.textContent = playerValue;
     return selected = 2;
   } else {
+    playerValue = "An error occurred";
+    playerPara.textContent = playerValue;
     return selected = -1;
   }
 }
 
 let winner = "";
-function playRound(computerSelection, playerSelection) {
-  if (playerSelection > -1) {
-    if (computerSelection < playerSelection) {
-      if (playerSelection === 2 && computerSelection === 0) {
-        //computer wins
-        winner = "computer";
-        return `The ${winner} wins! Rock beats scissors`;
-      } else if (playerSelection === 1) {
-        //user wins
-        winner = "player";
-        return `The ${winner} wins! Paper beats rock`;
-      } else {
-        winner = "player";
-        return `The ${winner} wins! Scissors beat paper`;
-      }
-    } else if (computerSelection > playerSelection){
-      if (computerSelection === 2 && playerSelection === 0) {
-        // user wins
-        winner = "player";
-        return `The ${winner} wins! Rock beats scissors`;
-      } else if (computerSelection === 1) {
-        // computer wins
-        winner = "computer";
-        return `The ${winner} wins! Paper beats rock`;
-      } else {
-        winner = "computer";
-        return `The ${winner} wins! Scissors beat paper`;
-      }
-    } else if (computerSelection === playerSelection) {
-      // draw
-      winner = "Nobody";
-      return `${winner} wins! It's a draw`;
+function playRound(e) {
+
+    const compSelection = computerSelection();
+    const playSelection = playerSelection(e);
+
+    let roundResult = "";
+
+    let compValue = "The computer's choice was ";
+    if (compSelection == 0) {
+      compValue += 'rock';
+    } else if (compSelection == 1) {
+      compValue += 'paper';
+    } else if (compSelection == 2) {
+      compValue += 'scissors';
     }
-  } else {
-    return "Wrong input.";
-  }
-}
 
-function game() {
-  let countCompWins = 0;
-  let countPlayWins = 0;
-  let gameWinner = "";
+    computerPara.textContent = compValue;
+    if (playSelection > -1) {
+      if (compSelection < playSelection) {
+        if (playSelection === 2 && compSelection === 0) {
+          //computer wins
+          winner = "computer";
+          roundResult = `The ${winner} wins this round! Rock beats scissors`;
+          resultPara.textContent = roundResult;
+        } else if (playSelection === 1) {
+          //user wins
+          winner = "player";
+          roundResult = `The ${winner} wins this round! Paper beats rock`;
+          resultPara.textContent = roundResult;
+        } else {
+          winner = "player";
+          roundResult = `The ${winner} wins this round! Scissors beat paper`;
+          resultPara.textContent = roundResult;
+        }
+      } else if (compSelection > playSelection){
+        if (compSelection === 2 && playSelection === 0) {
+          // user wins
+          winner = "player";
+          roundResult = `The ${winner} wins this round! Rock beats scissors`;
+          resultPara.textContent = roundResult;
+        } else if (compSelection === 1) {
+          // computer wins
+          winner = "computer";
+          roundResult = `The ${winner} wins this round! Paper beats rock`;
+          resultPara.textContent = roundResult;
+        } else {
+          winner = "computer";
+          roundResult = `The ${winner} wins this round! Scissors beat paper`;
+          resultPara.textContent = roundResult;
+        }
+      } else if (compSelection === playSelection) {
+        // draw
+        winner = "Nobody";
+        roundResult = `${winner} wins this round! It's a draw`;
+        resultPara.textContent = roundResult;
+      }
+    } else {
+      resultPara.textContent ="Wrong input.";
+    }
 
-  for (let i = 0; i < 5; i ++) {
-    playRound(computerSelection(), playerSelection());
     if (winner === 'computer') {
-      countCompWins ++;
-    } else if (winner === 'player'){
+      countCompWins++
+      scorePara.textContent = `Score: Player: ${countPlayWins} Computer: ${countCompWins}`;
+
+    } else if (winner === 'player') {
       countPlayWins ++;
+      scorePara.textContent = `Score: Player: ${countPlayWins} Computer: ${countCompWins}`;
     }
-  }
-  if (countPlayWins > countCompWins) {
-    gameWinner = "player";
-  } else if (countPlayWins < countCompWins) {
-    gameWinner = "computer";
-  }
-  if (countPlayWins != countCompWins) {
-    return `Total tally for player: ${countPlayWins}\nTotal tally for computer: ${countCompWins}\nThe ${gameWinner} wins!`;
-  } else {
-    return `Total tally for player: ${countPlayWins}\nTotal tally for computer: ${countCompWins}\nIt's a draw!`;
-  }
+    if (countCompWins === 5 && countCompWins > countPlayWins) {
+      finalResultPara.textContent = "The computer wins the game!";
+      container.removeChild(para);
+      container.removeChild(smallDiv);
+    } else if (countPlayWins === 5 && countPlayWins > countCompWins) {
+      finalResultPara.textContent = "The player wins the game!";
+      container.removeChild(para);
+      container.removeChild(smallDiv);
+    }
 }
-game();
+
+const bigDiv = document.createElement('div');
+document.body.appendChild(bigDiv);
+const container = document.querySelector('#container');
+const mainHeader = document.createElement('h2');
+mainHeader.textContent = "Rock paper scissors game";
+container.appendChild(mainHeader);
+const para = document.createElement('p');
+container.appendChild(para);
+para.textContent = "Click on your choice:";
+const smallDiv = document.createElement('div');
+container.appendChild(smallDiv);
+
+const btnRock = document.createElement('button');
+btnRock.textContent = "Rock";
+const btnPaper = document.createElement('button');
+btnPaper.textContent = "Paper";
+const btnScissors = document.createElement('button');
+btnScissors.textContent = "Scissors";
+
+
+smallDiv.appendChild(btnRock);
+smallDiv.appendChild(btnPaper);
+smallDiv.appendChild(btnScissors);
+
+
+let countCompWins = 0;
+let countPlayWins = 0;
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => button.addEventListener('click', playRound));
+
+
+
+const computerPara = document.createElement('p');
+bigDiv.appendChild(computerPara);
+const playerPara = document.createElement('p');
+bigDiv.appendChild(playerPara);
+const resultPara = document.createElement('p');
+bigDiv.appendChild(resultPara);
+const scorePara = document.createElement('p');
+bigDiv.appendChild(scorePara);
+const finalResultPara = document.createElement('p');
+bigDiv.appendChild(finalResultPara);
